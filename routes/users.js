f5ff9c11a583942b668c61cc0689234ac0e9c0de
Tylan Drydale <113541994+Tdrydale@users.noreply.github.com>
2025-03-26 08:35:07 -0500

    Step 5

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var radio = 0.75;
+var acc = 1000;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  acc = radio * acc;
+  res.send('Value is:' + acc);
 });
 
 module.exports = router;
